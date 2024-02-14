228b3def55c225fa24322c01e356c47270c47818
felipevzps <felipe.vzps@gmail.com>
2024-02-14 00:51:47 -0200

    playing with barcodes

--- playground/3_adapter_removal/porechop_soft.sh
+++ playground/3_adapter_removal/porechop_soft.sh
@@ -8,4 +8,10 @@
 module load miniconda3
 conda activate metagenomics
 
-/usr/bin/time -v porechop -i ../2_trimmed/soft_filter/all.500k.trimmed.highquality.fastq -o soft_filter/all.500k.trimmed.highquality.noadapter.fastq --threads $NSLOTS
+# dont separate barcodes
+#/usr/bin/time -v porechop -i ../2_trimmed/soft_filter/all.500k.trimmed.highquality.fastq -o soft_filter/all.500k.trimmed.highquality.noadapter.fastq --threads $NSLOTS
+
+# separate files into barcodes
+/usr/bin/time -v porechop -i ../2_trimmed/soft_filter/all.500k.trimmed.highquality.fastq -b soft_filter_barcode --barcode_threshold 75 --barcode_diff 5 --adapter_threshold 95 --threads $NSLOTS
+# --barcode_threshold 80
+# Error: no barcodes were found, so Porechop cannot perform barcode demultiplexing
