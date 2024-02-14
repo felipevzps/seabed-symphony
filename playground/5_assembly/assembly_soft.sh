2532e50f456d1e426779e25c9b64275e27915389
felipevzps <felipe.vzps@gmail.com>
2024-02-14 03:51:42 -0200

    fixed soft assembly

--- playground/5_assembly/assembly_soft.sh
+++ playground/5_assembly/assembly_soft.sh
@@ -3,9 +3,9 @@
 #$ -q all.q
 #$ -V
 #$ -cwd
-#$ -pe smp 10
+#$ -pe smp 20
 
 module load miniconda3
 conda activate metagenomics
 
-/usr/bin/time -v flye --meta --nano-raw ../3_adapter_removal/soft_filter/all.500k.trimmed.highquality.noadapter.fastq --out-dir soft_filter --threads $NSLOTS
+/usr/bin/time -v flye --meta --nano-hq ../3_adapter_removal/soft_filter/all.500k.trimmed.highquality.noadapter.fastq --out-dir soft_filter --threads $NSLOTS
