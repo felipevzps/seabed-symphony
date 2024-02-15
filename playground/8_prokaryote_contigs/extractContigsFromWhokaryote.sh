a9c0a17768d6879592f7d22b1b0e70c39ebb6632
felipevzps <felipe.vzps@gmail.com>
2024-02-15 01:53:09 -0200

    run extractContigsFromWhokaryote.py

---
+++ playground/8_prokaryote_contigs/extractContigsFromWhokaryote.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
+
+module load miniconda3
+conda activate metagenomics
+
+# Extract prokaryote contigs from assembly
+/usr/bin/time -v ./extractContigsFromWhokaryote.py -i ../5_assembly/soft_filter/assembly.fasta -p ../7_assign_taxonomy/whokaryote_output/prokaryote_contig_headers.txt -o prokaryote
+
+# Extract eukaryote contigs from assembly
+/usr/bin/time -v ./extractContigsFromWhokaryote.py -i ../5_assembly/soft_filter/assembly.fasta -p ../7_assign_taxonomy/whokaryote_output/eukaryote_contig_headers.txt -o eukaryote
