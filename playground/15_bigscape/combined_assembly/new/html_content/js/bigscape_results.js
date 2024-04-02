d0586d111d95916b076f0ec2c79062e7bf3f0c98
felipevzps <felipe.vzps@gmail.com>
2024-04-02 22:52:57 +0200

    add genome names

---
+++ playground/15_bigscape/combined_assembly/new/html_content/js/bigscape_results.js
@@ -0,0 +1,37 @@
+var bigscape_results = [
+    {
+        "label":"2024-04-02_08-17-00_hybrids_auto_c0.30",
+        "networks":[
+            {
+                "css":"PKSI",
+                "label":"PKSI",
+                "name":"PKSI"
+            },
+            {
+                "css":"Others",
+                "label":"Others",
+                "name":"Others"
+            },
+            {
+                "css":"NRPS",
+                "label":"NRPS",
+                "name":"NRPS"
+            },
+            {
+                "css":"PKSother",
+                "label":"PKSother",
+                "name":"PKSother"
+            },
+            {
+                "css":"RiPPs",
+                "label":"RiPPs",
+                "name":"RiPPs"
+            },
+            {
+                "css":"Terpene",
+                "label":"Terpene",
+                "name":"Terpene"
+            }
+        ]
+    }
+];
