d27b104c3386d5c624fbf03fae3802291690eebd
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:41:49 +0200

    raw barcode02

---
+++ playground/1_raw_qc/thirdRun/barcode02/nanoplot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J nanoplot
+#BSUB -n 5
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 50
+#BSUB -o nanoplot.sh.o%J
+#BSUB -e nanoplot.sh.e%J
+
+source activate metagenomics
+
+/usr/bin/time -v NanoPlot -t 5 --fastq ../../../0_raw/third_run/barcode02/barcode02.fastq --plots dot --legacy dot --N50 -o nanoplot
