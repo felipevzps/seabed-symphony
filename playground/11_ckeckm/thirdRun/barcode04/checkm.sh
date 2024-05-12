ce8ffc434df7ba14aa550144bcdb3541b5c2a2d8
felipevzps <felipe.vzps@gmail.com>
2024-05-13 01:31:31 +0200

    checkm barcode04

---
+++ playground/11_ckeckm/thirdRun/barcode04/checkm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J checkm
+#BSUB -n 10
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -W 3:00
+#BSUB -o checkm.sh.o%J
+#BSUB -e checkm.sh.e%J
+
+source activate checkm
+
+bins=../../../10_non-redundant_bins/third_run/barcode04/output/DASToolRun_DASTool_bins/
+
+/usr/bin/time -v checkm lineage_wf -x fa -t 10 --tab_table $bins output/ -f output/checkm.tsv 
+
