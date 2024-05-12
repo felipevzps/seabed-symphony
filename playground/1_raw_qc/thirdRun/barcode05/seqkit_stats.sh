e6d4c745ac98744ef4a40fe9293db9a0bd969e99
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:42:42 +0200

    raw barcode05

---
+++ playground/1_raw_qc/thirdRun/barcode05/seqkit_stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J seqkit_stats
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 15
+#BSUB -o seqkit_stats.sh.o%J
+#BSUB -e seqkit_stats.sh.e%J
+
+source activate metagenomics
+
+/usr/bin/time -v seqkit stats ../../../0_raw/third_run/barcode05/barcode05.fastq >> seqkit_stats.txt
