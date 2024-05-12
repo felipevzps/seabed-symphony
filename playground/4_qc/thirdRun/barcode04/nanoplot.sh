1979b0ca9b70221c21184bdc727ee2f33c094ada
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:51:33 +0200

    trimmed barcode04

---
+++ playground/4_qc/thirdRun/barcode04/nanoplot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J nanoplot
+#BSUB -n 5
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -W 50
+#BSUB -o nanoplot.sh.o%J
+#BSUB -e nanoplot.sh.e%J
+
+source activate metagenomics
+
+/usr/bin/time -v NanoPlot -t 5 --fastq ../../../3_adapter_removal/third_run/barcode04/soft_filter/barcode04.trimmed.highquality.noadapter.fastq/BC04.fastq --plots dot --legacy dot --N50 -o nanoplot
