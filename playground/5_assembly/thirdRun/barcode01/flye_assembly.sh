99a769e7c2a0155518de5bc8e8baadde8f3997c3
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:53:33 +0200

    assembly barcode01

---
+++ playground/5_assembly/thirdRun/barcode01/flye_assembly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J flye_assembly
+#BSUB -n 10
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -W 30:00
+#BSUB -o flye_assembly.sh.o%J
+#BSUB -e flye_assembly.sh.e%J
+
+source activate metagenomics
+
+/usr/bin/time -v flye --meta --nano-hq ../../../3_adapter_removal/third_run/barcode01/soft_filter/barcode01.trimmed.highquality.noadapter.fastq/BC01.fastq --out-dir barcode01 --iterations 2 --threads 10 
