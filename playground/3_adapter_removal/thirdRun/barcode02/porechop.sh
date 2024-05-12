25ac544d9429288250cb3ef67f94b7c9c073264d
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:47:20 +0200

    porechop barcode02

---
+++ playground/3_adapter_removal/thirdRun/barcode02/porechop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J porechop
+#BSUB -n 5
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -W 10:00
+#BSUB -o porechop.sh.o%J
+#BSUB -e porechop.sh.e%J
+
+source activate metagenomics
+
+mkdir soft_filter
+
+/usr/bin/time -v porechop -i ../../../2_trimmed/third_run/barcode02/soft_filter/barcode02.trimmed.highquality.fastq -b soft_filter/barcode02.trimmed.highquality.noadapter.fastq --threads 5 
