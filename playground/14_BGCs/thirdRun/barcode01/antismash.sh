0175afdd2015d2a9be9927451f14d61d79bba6d8
felipevzps <felipe.vzps@gmail.com>
2024-05-13 01:35:02 +0200

    antismash barcode01

---
+++ playground/14_BGCs/thirdRun/barcode01/antismash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J antismash[1-1]%2
+#BSUB -n 20
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -W 72:00
+#BSUB -o antismash.sh.o%J
+#BSUB -e antismash.sh.e%J
+
+source activate antismash
+
+bakta_annot=../../../13_bakta/third_run/barcode01/output/
+genome=`ls -1 $bakta_annot | head -n $LSB_JOBINDEX | tail -n 1`
+genbank=../../../13_bakta/third_run/barcode01/output/${genome}/${genome}.gbff
+
+mkdir -p output
+mkdir output/$genome
+
+/usr/bin/time -v antismash $genbank --databases /work3/fevape/database/antismash -t bacteria -c 20 --output-dir output/$genome --output-basename $genome --clusterhmmer --cb-general --cb-knownclusters --pfam2go
