7f918ca4b26926e2d4fe4ce2f7f4bf1081d3457a
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:44:15 +0200

    filtlong barcode1

---
+++ playground/2_trimmed/thirdRun/barcode01/filtlong.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J filtlong
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 50
+#BSUB -o filtlong.sh.o%J
+#BSUB -e filtlong.sh.e%J
+
+source activate metagenomics
+
+mkdir soft_filter
+
+/usr/bin/time -v filtlong --min_length 1000 ../../../0_raw/third_run/barcode01/barcode01.fastq >> soft_filter/barcode01.trimmed.fastq
