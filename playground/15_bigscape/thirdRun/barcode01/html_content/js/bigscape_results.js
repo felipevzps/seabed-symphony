3296204acaa36a22e57b01bef03b43c8d45590db
felipevzps <felipe.vzps@gmail.com>
2024-05-13 01:38:32 +0200

    bigscape barcode01

---
+++ playground/15_bigscape/thirdRun/barcode01/html_content/js/bigscape_results.js
@@ -0,0 +1,6 @@
+var bigscape_results = [
+    {
+        "label":"2024-05-11_13-01-55_hybrids_auto_c0.30",
+        "networks":[]
+    }
+];
---
+++ playground/15_bigscape/thirdRun/barcode01/html_content/networks/2024-05-11_13-01-55_hybrids_auto_c0.30/run_data.js
@@ -0,0 +1,14 @@
+var run_data={
+    "duration":"6h28m36s",
+    "end_time":"11/05/2024 19:30:31",
+    "input":{
+        "accession":[],
+        "accession_newick":[],
+        "bgc":[],
+        "classes":[]
+    },
+    "networks":[],
+    "parameters":"-i gbks -o output --pfam_dir /work3/fevape/database/pfam -c 20 --verbose --include_singletons --mode auto --mibig",
+    "start_time":"11/05/2024 13:01:55"
+};
+dataLoaded();
