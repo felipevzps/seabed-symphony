833cbf0316f6e658a61a61bce11498760579e286
felipevzps <felipe.vzps@gmail.com>
2024-05-13 00:57:33 +0200

    whokaryote barcode04

---
+++ playground/7_assign_taxonomy/thirdRun/barcode04/whokaryote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J whokaryote
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -W 10:00
+#BSUB -o whokaryote.sh.o%J
+#BSUB -e whokaryote.sh.e%J
+
+source activate whokaryote
+
+/usr/bin/time -v whokaryote.py --contigs ../../../5_assembly/third_run/barcode04/barcode04/assembly.fasta --outdir output --threads 4
