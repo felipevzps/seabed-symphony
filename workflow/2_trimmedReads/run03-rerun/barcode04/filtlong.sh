650ad06c522cc335ae5edd6953789fd53055723f
felipevzps <felipe.vzps@gmail.com>
2025-03-30 04:33:12 +0200

    add run03-rerun

---
+++ workflow/2_trimmedReads/run03-rerun/barcode04/filtlong.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J filtlong
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 50
+#BSUB -o filtlong.sh.o%J
+#BSUB -e filtlong.sh.e%J
+
+source activate metagenomics
+
+mkdir soft_filter
+
+input="/work3/fevape/rerun-analysis/0_rawReads/run03/barcode04/barcode04.fastq"
+
+/usr/bin/time -v filtlong --min_length 1000 $input  >> soft_filter/barcode04.trimmed.fastq
---
+++ workflow/2_trimmedReads/run03-rerun/barcode04/filtlong_highquality.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J filtlong_highquality
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 50
+#BSUB -o filtlong_highquality.sh.o%J
+#BSUB -e filtlong_highquality.sh.e%J
+
+source activate metagenomics
+
+/usr/bin/time -v filtlong --keep_percent 90 soft_filter/barcode04.trimmed.fastq >> soft_filter/barcode04.trimmed.highquality.fastq  
+
+rm -f soft_filter/barcode04.trimmed.fastq
