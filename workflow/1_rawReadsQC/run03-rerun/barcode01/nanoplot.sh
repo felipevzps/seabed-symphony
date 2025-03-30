9b74fbc2e2c5b728f37b59e16f19b926942a4164
felipevzps <felipe.vzps@gmail.com>
2025-03-30 04:30:33 +0200

    add run03-rerun

---
+++ workflow/1_rawReadsQC/run03-rerun/barcode01/nanoplot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J nanoplot
+#BSUB -n 5
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 50
+#BSUB -o nanoplot.sh.o%J
+#BSUB -e nanoplot.sh.e%J
+
+source activate metagenomics
+
+input="/work3/fevape/rerun-analysis/0_rawReads/run03/barcode01/barcode01.fastq"
+
+/usr/bin/time -v NanoPlot -t 5 --fastq $input  --plots dot --legacy dot --N50 -o nanoplot
