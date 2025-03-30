993a7be8d776ce22cb34b3b3a5b1e523d1593b49
felipevzps <felipe.vzps@gmail.com>
2025-03-30 04:45:10 +0200

    add run03-rerun

---
+++ workflow/8_prokaryoteContigs/run03-rerun/barcode02/extractProkaryoteContigsFromWhokaryote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#BSUB -q hpc
+#BSUB -J extract_whokaryote
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 15
+#BSUB -o whokaryote.sh.o%J
+#BSUB -e whokaryote.sh.e%J
+
+source activate metagenomics
+
+assembly="/work3/fevape/rerun-analysis/5_metagenomeAssembly/run03/barcode02/assembly/assembly.fasta"
+headers="/work3/fevape/rerun-analysis/7_assignTaxonomy/run03/barcode02/output/prokaryote_contig_headers.txt"
+
+/usr/bin/time -v /work3/fevape/rerun-analysis/bin/./extractContigsFromWhokaryote.py --i $assembly --p $headers --o prokaryote
